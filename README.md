27462744aed7056dc51eb37ba896e828081f3cd0
Sandrino Di Mattia <contact@sandrino.be>
2017-01-16 22:35:11 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
-# jwt-webtask-compiler
-A webtask compiler that will validate your JWT tokens before executing your webtask
+# Webtask Compiler for Auth0 access tokens
+
+This webtask compiler will validate your Auth0 access tokens before executing your webtask. You can read more about [Webtask compilers](https://webtask.io/docs/webtask-compilers) here.
+
+## Usage
+
+In order to use this Webtask compiler you have to specify it when creating your webtask:
+
+```bash
+wt create hello.js \
+  --meta wt-compiler=https://cdn.rawgit.com/sandrinodimattia/auth0-jwt-webtask-compiler/master/compiler.js
+  --secret AUTH0_DOMAIN=YOUR_AUTH0_DOMAIN
+  --secret AUTH0_AUDIENCE=YOUR_AUTH0_AUDIENCE
+```
+
+After setting up this compiler, you can send your access token to your webtask:
+
+```bash
+curl -X GET -H "Authorization: Bearer eyJ0..." -H "Content-Type: application/json" "https://you.run.webtask.io/hello"
+```
+
+The compiler will validate the signature of the token, the audience and the issuer. As a final step it will set the decoded token on `context.user` and `req.user` after which your Webtask will be executed.
+
+## Example Webtasks
+
+Using the simple signature:
+
+```js
+module.exports = function(ctx, cb) {-
+  cb(null, 'Hello ' + ctx.user.sub + '. Here are your scopes: ' + ctx.user.scope);
+}
+```
+
+Using request/response:
+
+```js
+module.exports = function(ctx, req, res) {-
+  res.writeHead(200);
+  res.end('Hello ' + req.user.sub + '. Here are your scopes: ' + req.user.scope);
+}
+```
+
+## How do I get an access token from Auth0?
+
+Start by creating an API [here](https://manage.auth0.com/#/apis). The `Identifier` you set here is the value you will use for `AUTH0_AUDIENCE` (eg: `urn:myapi`).
+
+Then using the code flow, implicit flow, client credentials or password grant you can get an access token by setting `audience=urn:myapi` for example. The resulting access token can then be sent to your Webtask as a bearer token.
---
+++ compiler.js
@@ -0,0 +1,140 @@
+const jwt = require('jsonwebtoken');
+const jwksClient = require('jwks-rsa');
+
+const jwksClients = { };
+
+const validateToken = function(ctx, headers, cb) {
+  try {
+    if (!ctx.secrets.AUTH0_DOMAIN || ctx.secrets.AUTH0_DOMAIN === '') {
+      return cb({
+        status: 500,
+        error: 'InternalServerError',
+        error_description: 'The AUTH0_DOMAIN setting is missing.'
+      });
+    }
+
+    if (!ctx.secrets.AUTH0_AUDIENCE || ctx.secrets.AUTH0_AUDIENCE === '') {
+      return cb({
+        status: 500,
+        error: 'InternalServerError',
+        error_description: 'The AUTH0_AUDIENCE setting is missing.'
+      });
+    }
+
+    if (!headers || !headers.authorization || headers.authorization === '') {
+      return cb({
+        status: 401,
+        error: 'UnauthorizedError',
+        error_description: 'Authorization header is missing.'
+      });
+    }
+
+    var authorizationHeader = headers.authorization.split(' ');
+    if (authorizationHeader.length !== 2 || authorizationHeader[0] !== 'Bearer') {
+      return cb({
+        status: 401,
+        error: 'UnauthorizedError',
+        error_description: 'Authorization header is invalid.'
+      });
+    }
+
+    var client = jwksClients[ctx.secrets.AUTH0_DOMAIN];
+    if (!client) {
+      client = jwksClient({
+        cache: true,
+        jwksUri: 'https://' + ctx.secrets.AUTH0_DOMAIN + '/.well-known/jwks.json'
+      });
+      jwksClients[ctx.secrets.AUTH0_DOMAIN] = client;
+    }
+
+    var token = authorizationHeader[1];
+    var decoded = jwt.decode(token, { complete: true });
+    if (!decoded.header || decoded.header.alg !== 'RS256') {
+      return cb({
+        status: 401,
+        error: 'UnauthorizedError',
+        error_description: 'Only tokens signed using RS256 are supported.'
+      });
+    }
+
+    if (!decoded.header.kid || decoded.header.kid.length === 0) {
+      return cb({
+        status: 401,
+        error: 'UnauthorizedError',
+        error_description: 'Token is missing the \'kid\' attribute.'
+      });
+    }
+
+    return client.getSigningKey(decoded.header.kid, (err, key) => {
+      if (err) {
+        return cb({
+          status: 401,
+          error: 'UnauthorizedError',
+          error_description: err.message
+        });
+      }
+
+      var validationOptions = {
+        audience: ctx.secrets.AUTH0_AUDIENCE,
+        issuer: 'https://' + ctx.secrets.AUTH0_DOMAIN + '/'
+      };
+      jwt.verify(token, key.publicKey || key.rsaPublicKey, validationOptions, function(err, decoded) {
+        if (err) {
+          return cb({
+            status: 401,
+            error: 'UnauthorizedError',
+            error_description: err.message
+          });
+        }
+
+        return cb(null, decoded);
+      });
+    });
+  } catch (e) {
+    return cb({
+      status: 500,
+      error: 'InternalServerError',
+      error_description: e.message
+    });
+  }
+};
+
+module.exports = function (options, cb) {
+  options.nodejsCompiler(options.script, function (error, compiledFunc) {
+    if (error) {
+      return cb(error);
+    }
+
+    var func = compiledFunc;
+    if (func.length === 3) {
+      func = function(ctx, req, res) {
+        validateToken(ctx, req.headers, function(err, user) {
+          if (err) {
+            res.writeHead(err.status, { 'Content-Type': 'application/json' });
+            res.end(JSON.stringify(err, null, 2));
+            return;
+          }
+
+          ctx.user = user;
+          req.user = user;
+          compiledFunc(ctx, req, res);
+        });
+      };
+    } else if (func.length === 2) {
+      func = function(ctx, callback) {
+        validateToken(ctx, ctx.headers, function(err, user) {
+          if (err) {
+            return callback(err.error + ': ' + err.error_description);
+          }
+
+          ctx.user = user;
+          compiledFunc(ctx, callback);
+        });
+      };
+    } else {
+      cb(new Error('Signature not compatible.'));
+    }
+
+    return cb(null, func);
+  });
+};
